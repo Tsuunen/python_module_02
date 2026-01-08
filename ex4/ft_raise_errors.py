db6f24b53c1fb01f6435a6a647bc956ffc758d01
Remi Laforgue <relaforg@z3r6p3.42lyon.fr>
2026-01-08 11:23:11 +0100

    fix ex4

--- ex4/ft_raise_errors.py
+++ ex4/ft_raise_errors.py
@@ -4,38 +4,41 @@ def check_plant_health(plant_name, water_level, sunlight_hours):
         print("Error: Plant name cannot be empty!")
         raise ValueError
     elif (water_level < 1):
-        print(f"Error: Water level {water_level} is too low (min 1)")
-        raise ValueError
+        raise ValueError(f"Error: Water level {water_level} is too low \
+(min 1)")
     elif (water_level > 10):
-        print(f"Error: Water level {water_level} is too high (max 10)")
-        raise ValueError
+        raise ValueError(
+            f"Error: Water level {water_level} is too high (max 10)")
     elif (sunlight_hours < 2):
-        print(f"Error: Sunlight hours {sunlight_hours} is too low (min 2)")
-        raise ValueError
+        raise ValueError(
+            f"Error: Sunlight hours {sunlight_hours} is too low (min 2)")
     elif (sunlight_hours > 12):
-        print(f"Error: Sunlight hours {sunlight_hours} is too high (max 12)")
-        raise ValueError
-    print(f"Plant '{plant_name}' is healthy!")
+        raise ValueError(
+            f"Error: Sunlight hours {sunlight_hours} is too high (max 12)")
+    return (f"Plant '{plant_name}' is healthy!")
 
 
 def test_plant_checks():
     """test the check_plant_health() function"""
     print("=== Garden Plant Health Checker ===\n")
     print("Testing good values...")
-    check_plant_health("tomato", 1, 2)
+    print(check_plant_health("tomato", 1, 2))
     print("\nTesting empty plant name...")
     try:
-        check_plant_health("", 1, 2)
-    except ValueError:
-        pass
-    print("\nTesting bad water level...")
+        print(check_plant_health("", 1, 2))
+    except ValueError as e:
+        print(e)
+    print("Testing bad water level...")
     try:
-        check_plant_health("tomato", 15, 2)
-    except ValueError:
-        pass
+        print(check_plant_health("tomato", 15, 2))
+    except ValueError as e:
+        print(e)
     print("\nTesting bad sunlight hours...")
     try:
-        check_plant_health("tomato", 1, 0)
-    except ValueError:
-        pass
+        print(check_plant_health("tomato", 1, 0))
+    except ValueError as e:
+        print(e)
     print("\nAll error raising tests completed!")
+
+
+test_plant_checks()
